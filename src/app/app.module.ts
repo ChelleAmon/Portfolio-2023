b28222bb779f3eb08717332e62b5d5724f2ab427
Gichelle Amon <81938681+ChelleAmon@users.noreply.github.com>
2022-11-15 10:47:29 -0800

    Create other components pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { HeaderComponent } from './components/contents/header/header/header.component';
+import { AboutComponent } from './components/contents/about/about/about.component';
+import { SkillsComponent } from './components/contents/skills/skills/skills.component';
+import { ProjectsComponent } from './components/contents/projects/projects/projects.component';
+import { ContactComponent } from './components/contents/contact/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavigationComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
