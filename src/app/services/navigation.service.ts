eab7f3963e4e1bb33fae6326314c6980e916c488
Gichelle Amon <81938681+ChelleAmon@users.noreply.github.com>
2022-12-29 19:19:17 -0800

    feat(hamburger-menu): generate service for navbar

---
+++ src/app/services/navigation.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavigationService {
+
+  constructor() { }
+
+  testfunction(){
+    console.log('Navigation for testing only!!!!!!!!!!!!')
+  }
+}
