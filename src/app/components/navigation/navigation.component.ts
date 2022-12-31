0d27260917c6c21bdd5153db4e549d25d00e8a6f
Gichelle Amon <81938681+ChelleAmon@users.noreply.github.com>
2022-12-30 22:26:24 -0800

    feat(animation): add animation metadata property

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,10 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationService } from 'src/app/services/navigation.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
+  animations: [
+    trigger('openClose', [
+
+      state('open', style({
+        display: 'block',
+      })),
+
+      state('close', style({
+        display: 'none'
+      })),
+
+      transition('open => closed', [
+        animate("3s ease-in")
+      ]),
+      transition('closed => open', [
+        animate("0s")
+      ]),
+    ]),
+  ]
+
+//   // On click
+// // Open overlay: "About";
+// // Animate: Move in Bottom;
+// animation-timing-function: ease-in;
+// animation-duration: 300ms;
+
+// // While hovering
+// // Open overlay: "About";
+// // Animate: Instant;
+// animation-duration: 0ms;
+
 })
 export class NavigationComponent implements OnInit {
   clicked: Boolean;
