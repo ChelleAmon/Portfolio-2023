b5a26c9252e5d2b398ebe97168aecc871c6a10ad
Gichelle Amon <81938681+ChelleAmon@users.noreply.github.com>
2023-01-02 20:44:22 -0800

    feat(contact): bind img src property

--- src/app/components/contents/contact/contact/contact.component.ts
+++ src/app/components/contents/contact/contact/contact.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  contact_me_image = "../../../../../assets/uploads//contact-me.png";
+
 
   constructor() { }
 
