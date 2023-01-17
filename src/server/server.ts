f3202eb5181343997deb8d1cc809c4184f8ce52e
Gichelle Amon <amongichelle@gmail.com>
2023-01-16 21:33:30 -0800

    feat(mail): set up email credentials

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,29 +3,76 @@ import cors from "cors";
 import http from "http";
 import dotenv from "dotenv";
 import path from "path";
+import nodemailer from "nodemailer";
 
 dotenv.config();
 
 const __dirname = path.resolve();
 const app = express();
 const clientPath = path.join(__dirname, '/dist/client');
+const PORT = 3000; // temporary. Save it in the env file
+const mail_user = {
+    test_emailfrom: process.env.TEST_EMAILFROM,
+    password: process.env.PASSWORD,
+    emailto: process.env.EMAILTO
+}
 
 
-app.use(express.static(clientPath))
+app.use(express.static(clientPath));
 app.use(cors({
     credentials: true,
     origin: "*"
-}))
-
-const PORT = 3000; // temporary. Save it in the env file
-
+}));
 app.use(express.json());
+
+const sendMail = (user: any, callback: any) => {
+    const transporter = nodemailer.createTransport({
+        /*
+        // I will keep these mail properties for now. 
+        // host: "smtp.gmail.com",
+        // port: 587,
+        // secure: true,
+        */
+        service: 'gmail',
+        auth: {
+            type: 'OAUTH2',
+            user : mail_user.test_emailfrom,
+            pass: mail_user.password,
+            clientId: process.env.OAUTH_CLIENTID,
+            clientSecret: process.env.OAUTH_CLIENT_SECRET,
+            refreshToken: process.env.OAUTH_REFRESH_TOKEN
+        }
+    })
+    const mailOptions = {
+        from: mail_user.test_emailfrom,
+        to: mail_user.emailto,
+        subject: "Test nodemail email",
+        text: "Whyyyyyyyyyyyy?? why is this working?"
+    }
+    transporter.sendMail(mailOptions, callback)
+
+}
+
 
 app.get("/", (req, res) => {
     res.send("Congratulations! Server is running successfully.")
+});
+
+
+app.post("/sendmail", (req,res) => {
+    console.log("request came");
+    let user: string = req.body;
+    sendMail(user, (err: any, info: any) => {
+            if (err) {
+                console.log("Error: ", err);
+                res.status(400);
+                res.send({error: "Failed to send email"})
+            } else {
+                console.log("Email has been sent")
+                res.send(info)
+            }
+        })
 })
-
-
 app.listen(PORT, () => {
     console.log(`Success! Starting localhost http://localhost:${PORT}`)
 
