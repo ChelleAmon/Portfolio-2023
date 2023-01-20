eeb9abdc54b38b97cfe6a84ab97082139a0a48ba
Gichelle Amon <amongichelle@gmail.com>
2023-01-19 21:38:31 -0800

    feat(services, client): create services and inject httpclientmodule to connect to backend

---
+++ src/client/src/app/services/contactus.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactusService {
+
+  constructor(private api: ApiService) { }
+
+  sendEmail(emailData: any){
+    return this.api.post('sendMail', emailData)
+  }
+}
