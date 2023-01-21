3ee37ad88c2df5efa7d9de4377b0acdc47483f28
Gichelle Amon <amongichelle@gmail.com>
2023-01-20 18:15:27 -0800

    feat(contactus): subscribe to contactService and pass data

--- src/client/src/app/components/contents/contact/contact/contact.component.ts
+++ src/client/src/app/components/contents/contact/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ContactusService } from 'src/app/services/contactus.service';
 
 @Component({
   selector: 'app-contact',
@@ -11,14 +12,14 @@ export class ContactComponent implements OnInit {
   contactForm: FormGroup;
   contact_me_image = "../../../../../assets/uploads//contact-me.png";
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private contactusService: ContactusService) {
 
     this.contactForm = this.fb.group({
-      email: [
+      from: [
         '',
        Validators.compose([ Validators.required, Validators.email ])
       ],
-      message: [
+      html: [
         '',
        Validators.compose([ Validators.required, Validators.minLength(3)])
       ]
@@ -29,15 +30,49 @@ export class ContactComponent implements OnInit {
   }
 
   sendEmail(){
-   if(this.contactForm.valid){
-    console.log('Email Successfully sent');
-    console.log('Email: ', this.contactForm.value.email)
-    console.log('message: ', this.contactForm.value.message)
-    this.contactForm.reset();
-   }
+  //  if(this.contactForm.valid){
+  //   console.log('Email Successfully sent');
+  //   console.log('Email: ', this.contactForm.value.email)
+  //   console.log('message: ', this.contactForm.value.message)
+  //   this.contactForm.reset();
+  //  }
+    if (this.contactForm.valid){
+      let contactUs = {
+        from: this.contactForm.value.from,
+        html: this.contactForm.value.html
+      }
+
+      this.contactusService.sendEmail(contactUs).subscribe({
+        next: (data: any) => console.log(data),
+        error: (err) => console.log("Errorzzzzz: ", err ),
+        complete: () => this.contactForm.reset()
+      })
+
+      this.contactForm.reset();
+    }else {
+      console.log('One of the fields is invalid!')
+    }
   }
 
   resetEmail(){
     this.contactForm.reset();
   }
 }
+
+// this.emailService.sendEmail(user).subscribe(
+//   data => {
+//     let res:any = data;
+//   },
+//   err => {
+//     this.loading = false;
+//     this.buttonText = "Submit";
+//   },() => {
+//     this.loading = false;
+//     this.buttonText = "Submit";
+//     this.nameFormControl.reset();
+//     this.emailFormControl.reset();
+//     this.subjectFormControl.reset();
+//     this.textareaFormControl.reset();
+//   }
+// );
+// }
