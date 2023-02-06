82a9b82e6084bfc19c70b8ffa275503b30af6593
Gichelle Amon <amongichelle@gmail.com>
2023-02-05 22:21:25 -0800

    feat(client): declare an array with series of objects that hold contribution data

--- src/client/src/app/components/contents/contributions/contributions/projects.component.ts
+++ src/client/src/app/components/contents/contributions/contributions/projects.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  imgUrl = '../../../../../assets/uploads/projects-id.png';
 
-  constructor() { }
+  contributions = [
+    {
+      name: 'MKG Tax Consulting',
+      imgUrl: '/',
+      imgAlt: "Portfolio: MKG Tax Consulting screenshot",
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      technologies: "MEAN Stack, CSS, Heroku",
+      website: "https://mkgtaxconsultants.com/",
+    },
+    {
+      name: 'Foundation for FUSD',
+      imgUrl: '/',
+      imgAlt: "Portfolio: Foundations for FUSD screenshot",
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      website: "https://foundation4fusd.org/",
+    },
+    {
+      name: 'The Elementals',
+      imgUrl: '/',
+      imgAlt: "Portfolio: The Elementals screenshot",
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      website: "/",
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
