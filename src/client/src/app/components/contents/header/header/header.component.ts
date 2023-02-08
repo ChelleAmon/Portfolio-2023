870807c2adabb491d28e3dc9cf241c97fe31832a
Gichelle Amon <amongichelle@gmail.com>
2023-02-07 19:53:11 -0800

    feat(home): add anchor tag properties and their default values as an object

--- src/client/src/app/components/contents/header/header/header.component.ts
+++ src/client/src/app/components/contents/header/header/header.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  socialMediaAnchorProps = {
+    target: "_blank",
+    rel: "noopener noreferrer"
+  }
+
   constructor() { }
 
   ngOnInit(): void {
