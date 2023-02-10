c91bd2b61682f0cbfbd5acfea8307467bbd88ef3
Gichelle Amon <amongichelle@gmail.com>
2023-02-10 13:59:37 -0800

    chore(navigation): remove animation decorator

--- src/client/src/app/components/navigation/navigation.component.ts
+++ src/client/src/app/components/navigation/navigation.component.ts
@@ -6,25 +6,6 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
-  // animations: [
-  //   trigger('openClose', [
-
-  //     state('open', style({
-  //       display: 'block',
-  //     })),
-
-  //     state('close', style({
-  //       display: 'none'
-  //     })),
-
-  //     transition('open => close', [
-  //       animate("1s ease-in")
-  //     ]),
-  //     transition('close => open', [
-  //       animate("1s ease-out")
-  //     ]),
-  //   ]),
-  // ]
 })
 
 export class NavigationComponent implements OnInit {
