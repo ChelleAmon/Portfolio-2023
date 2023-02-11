5efae5584d21c365486754fe386ad5f9f1a1b293
Gichelle Amon <amongichelle@gmail.com>
2023-02-10 18:03:28 -0800

    ref(header): bind data of the profile image and other contents

--- src/client/src/app/components/contents/header/header/header.component.ts
+++ src/client/src/app/components/contents/header/header/header.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  profileProps= {
+    src: "../../../../../assets/uploads/IMG_5879.png",
+    alt: "Gichelle Portfolio- Greetings"
+  }
+
+  headerContents = {
+    name: "Gichelle Amon",
+    job: "Full-Stack Software Developer"
+  }
 
   socialMediaAnchorProps = {
     target: "_blank",
